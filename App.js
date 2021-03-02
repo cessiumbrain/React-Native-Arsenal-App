e74480d1704ff8ee9d37f326754536054fede8a4
Joe Iannotta <joeiannotta@joes-mbp.myfiosgateway.com>
2021-03-01 21:31:31 -0500

    commit 1

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Image, TextInput } from 'react-native';
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require('./0572.jpg')} style={styles.imageBackground}>
+        <Image source={require('./NYE.png')} style={styles.logo}></Image>
+        <TextInput
+          style={{ width: 200, height: 40, backgroundColor: 'white', borderColor: 'gray', borderWidth: 1, marginTop: 20 }}
+      
+    />
+        <TextInput
+          style={{ width: 200, height: 40, backgroundColor: 'white', borderColor: 'gray', borderWidth: 1 }}
+        
+        />
+      </ImageBackground>
     </View>
   );
 }
@@ -18,4 +29,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageBackground: {
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    resizeMode: "contain",
+    justifyContent: "center",
+    alignItems: 'center'
+  },
+  logo: {
+    height: 300,
+    width: 300
+
+  }
 });
